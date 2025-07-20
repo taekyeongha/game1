0da7cc7575124f517968b045f7381c7d1d2e0768
taekyeongha <htk6624@gmail.com>
2025-07-20 15:39:34 +0900

    2025.7.20 변수

---
+++ Class11th(Compile)/Source.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// main() 프로그램의 진입점을 역할을 하는 함수입니다.
+void main()
+{
+    // ; (세미 콜론) : 한 문장의 끝을 알려주는 기호입니다.
+    printf("League of Legend\n");
+
+    // \n (개행문자) : 해당하는 위치를 한 칸 아래로 내려주는 문자입니다.
+    printf("Game");
+}
