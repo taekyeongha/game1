ca512954841db29921f72fb1b617e9af05417576
taekyeongha <htk6624@gmail.com>
2025-07-21 19:50:48 +0900

    (2025.7.18) 오버클로우 & 언더플로우

--- Class3th(Bit)/Source.c
+++ Class3th(Bit)/Source.c
@@ -2,40 +2,57 @@
 
 void main()
 {
-#pragma region 오버플로우
-	// 특정한 자료형이 표현할 수 있는  최댓값의 범위를
-	// 넘어서 연산을 수행하는 과정입니다.
+#pragma region 조건문
+    // 어떤 조건이 주어질 때 해당 조건에 따라 동작을
+    // 수행하도록 실행하는 명령문입니다.
 
-	//char storage = 127 + 1;
+#pragma region 관계 연산자
+    // 두 개의 피연산자의 값을 비교하여 그 결과를 0 또는
+    // 1이라는 값으로 나타내는 연산자입니다.
 
-	//printf("stotage의 값 : %d\n", storage);
+    // int positionX = 10;
+    // int positionY = 20;
+    // 
+    // printf("positionX < positionY : %d\n", positionX < positionY);
+    // printf("positionX > positionY : %d\n", positionX > positionY);
+    // printf("positionX <= positionY : %d\n", positionX <= positionY);
+    // printf("positionX >= positionY : %d\n", positionX >= positionY);
+    // printf("positionX != positionY : %d\n", positionX != positionY);
+    // printf("positionX == positionY : %d\n", positionX == positionY);
 
-	// 오버플로우는 부호 없는 자료형에서 똑같이 발생하며,
-	// 실수일때 오버플로우가 발생하면 infinity라는 값으로 출력됩니다.
+    // 관계 연산자는 조건이 맞을 때 1이라는 값으로 반환되며,
+    // 조건이 틀릴 때 0이라는 값으로 반환됩니다.
 #pragma endregion
 
-#pragma region 언더플로우
-	// 특정한 자료형이 표현할 수 있는 최솟값의 범위를
-	// 넘어서 연산을 수행하는 과정입니다.
+#pragma region if문
+    // 어떤 특정한 조건을 비교하여 조건이 맞다면 실행되는
+    // 명령문입니다.
 
-	char data = - 128 - 1;
+    int health = 0;
 
-	printf("data의 값 : %d\n", data);
+    if (health <= 0)
+    {
+        printf(Destroy\n);
+    }
 
-	// 언더플로우는 부호 없는 자료형에서도 똑같이 발생하며,
-	// 실수일 때 언더플로우가 발생하면 0이라는 값으로 출력됩니다.
+    // if문은 조건이 1일 때 실행되며, 0일 때 실행되지 않습니다.
 #pragma endregion
 
-#pragma region 부호 없는 자료형
-	
-	// unsigned int count = 4294967295;
-	//
-	// //[1111 1111] [1111 1111] [1111 1111] [1111 1111]
-	// printf("signed count 변수의 값 : %d\n", count);
-	//
-	// //[1111 1111] [1111 1111] [1111 1111] [1111 1111]
-	// printf("unsigned  count 변수의 값 : %u\n",count);
+#pragma region else if문
+    // if문의 조건이 틀릴 때 else if 문의 조건이
+    // 맞다면 실행되는 명령문입니다.
 
+    int level = 99;
+
+    if (level < 11)
+    {
+        printf("1 Circles\n");
+    }
+    else
+
+        // else if문은 여러 번 정의할 수 있으며, if문의
+        // 존재할 때 사용할 수 있습니다.
 #pragma endregion
+
 
 }
