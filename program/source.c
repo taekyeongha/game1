ed60ee0aa88404553c6555b82a53efd23893fe69
sungjiin1064 <htk6624@gmail.com>
2025-07-29 19:45:50 +0900

    (2025.7.29) 배열

--- program/source.c
+++ program/source.c
@@ -2,151 +2,45 @@
 
 void main()
 {
-#pragma region 주소 연산자
-    // 변수의 주소 값을 반환하는 연산자입니다. 
+#pragma region 배열
+	// 같은 자료형의 변수들로 이루어진 유한 집합입니다.
 
-    // int data = 10;
+	// int list[5];
+	// 
+	// // 배열의 경우 첫 번째 원소는 0 부터 시작합니다.
+	// 
+	// for (int i = 0; i < 5; i++)
+	// {
+	// 	list[1] = (i + 1) * 10;
+	// 
+	// 	printf("list[%d] = %d\n", i, list[i]);
+	// }
 
-    // printf("data 변수의 주소: %p\n", &data);
+	// 배열의 메모리 공간은 프로그램의 실행되는 동안
+	// 변경할 수 없습니다.
 
-    // 데이터의 주소는 해당 데이터가 저장된 메모리의 시작 주소를
-    // 의미하며, 메모리 공간은 1 byte의 크기로 나누어 표현합니다.
-#pragma endregion
+	 int array[] = { 1,2,3,4,5 };
+	 
+	 int size = sizeof(array) / sizeof(array[0]);
+	 
+	 for (int i = 0; i < size; i++)
+	 {
+	 	printf("array[d]의 값 : %d\n", i, array[i]);
+	 }
 
-#pragma region scanf_s 함수
-    // 표준 입력 함수로, 여러 종류의 데이터를 다양한 서식에
-    // 맞추어 입력해주는 함수입니다.
+	// 배열의 크기는 생략할 수 있으며, 초기화 목록에서
+	// 설정한 요소에 따라 배열의 크가 결정됩니다.
 
-    // int x = 0;
+	   printf("배열의 시작 주소 : %p\n", array);
 
-    // printf("x 변수의 값을 입력 : ");
+	// 배열은 연속적인 메모리 공간을 가지고 있으며, 배열의 이름은
+	// 배열의 시작 주소를 가리킵니다.
 
-    // 표준 입력 함수는 입력을 수행할 때까지 다음 작업으로
-    // 넘어갈 수 없습니다.
+	int* pointer = 10;
+	int* pointer = 25;
 
-    // scanf_s("%d", &x);
-
-    // 버퍼는 데이터가 이동할 때 임시로 저장되는 공간이며,
-    // 서식 지정자에 따라 입력할 수 있는 데이터의 범위도 결정됩니다.
-
-    // printf("x 변수의 값 : %d\n", x);
-
-    // 표준 입력 함수로 데이터를 입력하게 되면 버퍼에
-    // 데이터를 보관하였다가 입력하는 순간 버퍼 안의
-    // 내용을 프로그램에 전송합니다.
-#pragma endregion
-
-#pragma region 포인터
-    // 메모리의 주소 값을 저장할 수 있는 변수입니다.
-
-    // int storage = 10;
-
-    // int variable = 20;
-
-    // int* pointer = &storage;
-
-    // 포인터가 가리키는 메모리 공간의 자료형은 알 수 없으므로,
-    // 포인터가 가리키는 메모리의 자료형을 선언해주어야 합니다.
-
-    // printf("storage 변수의 주소 : %p\n", &storage);
-    // printf("pointer 변수의 값 : %p\n", pointer);
-    // printf("pointer 변수의 주소 : %p\n\n", &pointer);
-
-    // *pointer = 99;
-
-    // printf("storage 변수의 값 : %d\n", storage);
-    // printf("pointer 변수의 가리키는 값 : %d\n", pointer);
-
-    // pointer = &variable;
-
-    // int variable = 100;
-
-    // printf("variable 변수의 값 : %d\n", variable);
-    // printf("variable 변수의 값 : %d\n", pointer);
-    // printf("pointer 변수가 가리키는 값 %d\n\n", pointer);
-
-    // 포인터 변수도 자신의 메모리 공간을 가지고 있으며,
-    // 포인터 변수에 변수의 주소를 저장하게 되면 해당 변수의
-    // 시작 주소를 가리키게 됩니다.
-
-    //  printf("int 자료형의 크기 ; %u\n", sizeof(int));
-    //  printf("pointer 변수의 크기 : %u\n", sizeof(pointer));
-
-    // 포인터 변수의 크기는 중앙 처리 장치가 한 번에 처리할 수
-    // 있는 크기로 정해지며, 한 번에 처리할 수 있는 크기는
-    // 운영체제에 따라 크기가 결정됩니다.
-#pragma endregion
-
-#pragma region 범용 포인터
-    // 자료형이 정해지지 않은 상태로 모든 자료형을
-    // 저장할 수 있는 포인터입니다.
-
-    // char rank = 'A';
-
-    // int count = 10;
-
-    // float angle = 3.14f;
-
-    // void* reference = NULL;
-
-    // reference = &rank;
-
-    // 범용 포인터는 메모리 주소에 접근해서 값을
-    // 변경할 수 없습니다.
-
-    // *(char*)reference = 'B';
-
-    // printf("rank 변수의 값 : %c\n", rank);
-    // printf("reference 변수의 값 : %p\n\n", reference);
-
-    // reference = &count;
-
-    // *(int*)reference = 50;
-
-    // printf("rank 변수의 값 : %d\n", count);
-    // printf("reference 변수의 값 : %p\n\n", reference);
-
-    // reference = &angle;
-
-    // *(float*)reference = 45.5f;
-
-    // printf(*"angle 변수의 값 : % f\n", angle);
-    //  printf("reference 변수의 값 : %p\n\n", reference);
-
-    // 범용 포인터로 변수의 메모리에 접근하려면 범용 포인터가
-    // 가리키는 변수의 자료형으로 형 변환해주어야 합니다.
-#pragma endregion
-
-#pragma region 상수 지시 포인터
-    // 포인터가 가리키는 주소에 저장되어 있는 값을 변경할 수 있도록
-    // 저장되어 있는 포인터입니다.
-
-    // int room1 = 10;
-    // int room2 = 20;
-
-    // const int* const_pointer = &room1;
-
-    // *const_pointer = 99;
-
-    // printf("room1 주소 값 %p\n", &room1);
-    // printf("const_pointer 값 %p\n", const_pointer);
-
-    // const_pointer = &room2;
-
-    // printf("room2 주소 값 %p\n", &room2);
-    // printf("const_pointer 값 %p\n", const_pointer);
-
-#pragma endregion
-
-#pragma region 포인터 상수
-    // 포인터 변수가 가리키고 있는 주소 값을
-    // 변경할 수 없는 포인터 입니다.
-     
-    int a = 99;
-
-    int b = 555;
-
-    int * count_reference;
+	printf("배열의 시작 주소 : %p\n", pointer);
+	printf("배열의 시작 주소 가리키는값 : %p\n", pointer);
 
 #pragma endregion
 
