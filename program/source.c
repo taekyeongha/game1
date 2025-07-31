4862b08181c175c5aa5dc47cc2df35288fcfd680
taekyeong6624 <htk6624@gmail.com>
2025-07-31 19:46:36 +0900

    (2025.7.30) 문자열

--- program/source.c
+++ program/source.c
@@ -1,47 +1,56 @@
 #include <stdio.h>
 
+void initialize()
+{
+	printf("initialize...\n");
+}
+
+int get_temperature()
+{
+	return 36.5f;
+}
+
+void update_position(int x, int y)
+{
+	printf("x - %d,y =%d\n", x, y);
+}
+
+void print_status(char grade, int height, float temperature)
+{
+	printf("등급 : %c\n", grade);
+	printf("키 : %c\n", height);
+	printf("체온 : %c\n", temperature);
+}
+
 void main()
 {
-#pragma region 배열
-	// 같은 자료형의 변수들로 이루어진 유한 집합입니다.
+#pragma region 함수
+	// 하나의 특별한 목적의 작업을 수행하기 위해
+	// 독립적으로 설계된 코드이 집합입니다.
 
-	// int list[5];
-	// 
-	// // 배열의 경우 첫 번째 원소는 0 부터 시작합니다.
-	// 
-	// for (int i = 0; i < 5; i++)
-	// {
-	// 	list[1] = (i + 1) * 10;
-	// 
-	// 	printf("list[%d] = %d\n", i, list[i]);
-	// }
+	// initialize();
+#pragma endregion
 
-	// 배열의 메모리 공간은 프로그램의 실행되는 동안
-	// 변경할 수 없습니다.
+#pragma region  반환형
+	// 함수가 실행을 끝내고 돌려주는 결과의 자료형입니다.
+	
+	// printf("온도 : %f\n", get_temperature());
 
-	 int array[] = { 1,2,3,4,5 };
-	 
-	 int size = sizeof(array) / sizeof(array[0]);
-	 
-	 for (int i = 0; i < size; i++)
-	 {
-	 	printf("array[d]의 값 : %d\n", i, array[i]);
-	 }
+	// 함수의 경우 자료형과 반환하는 값의 형태가
+	// 일치하지 않으면 원하는 값을 얻을 수 없습니다.
+#pragma endregion
 
-	// 배열의 크기는 생략할 수 있으며, 초기화 목록에서
-	// 설정한 요소에 따라 배열의 크가 결정됩니다.
+#pragma region 매개 변수
+	// 함수의 정의에서 전달받은 인수를 함수내부로
+	// 전달하기 위해 사용하는 변수입니다.
 
-	   printf("배열의 시작 주소 : %p\n", array);
+	update_position(36, -9);
 
-	// 배열은 연속적인 메모리 공간을 가지고 있으며, 배열의 이름은
-	// 배열의 시작 주소를 가리킵니다.
+	// 매개 변수는 함수 내부에서만 연산이 이루어지며,
+	// 함수가 종료되면 메모리에서 해제됩니다,
 
-	int* pointer = 10;
-	int* pointer = 25;
-
-	printf("배열의 시작 주소 : %p\n", pointer);
-	printf("배열의 시작 주소 가리키는값 : %p\n", pointer);
-
+	// 하나의 함수에 서로 다른 자료형의 매개 변수를 함께
+	// 정의할 수 있으며, 여러 개의 매개 변수를 생성할 수 있습니다.
 #pragma endregion
 
 
